d34624bc4df58a6914faf54594ee4373383be0d3
Rodrigo Vieira Eufrasio da Silva <rodrigo.mct@gmail.com>
2025-04-02 12:07:00 -0300

    make css page contact

--- partials/contact.php
+++ partials/contact.php
@@ -1,6 +1,6 @@
 <section class="contact paddingContent">
     <span class="subtitle">Fale Consco</span>
-    <?=get_the_title('<h1>','</h1>')?>
-    <p><?php the_content()?></p>
+    <h1><?=get_the_title()?></h1>
+    <?php the_content()?>
     <?=do_shortcode('[contact-form-7 id="5a54dc5" title="Formulário de contato 1"]')?>
 </section>
