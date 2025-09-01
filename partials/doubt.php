880b51fd7a41a85879ea0fdd4eef5850771f589b
Rodrigo Vieira E. Silva <rodrigo.vesilva>
2025-09-01 09:14:10 -0300

    news adjustment

--- partials/doubt.php
+++ partials/doubt.php
@@ -6,7 +6,7 @@
         <div class="content">
             <h2 itemprop="headline"><?php the_field('doubt_headline','option')?></h2>
             <?php $link = get_field('doubt_link', 'option')?>
-            <a href="<?= $link['url']?>" itemprop="url" class="btnMorePost"><?=$link['title']?></a>
+            <!-- <a href="<?= $link['url']?>" itemprop="url" class="btnMorePost"><?=$link['title']?></a> -->
         </div>        
     </article>
 </section>
