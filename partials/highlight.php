9afa80a31ad699905a49002c50e6f55893cfd202
Rodrigo Vieira Eufrasio da Silva <rodrigo.mct@gmail.com>
2025-09-02 20:42:06 -0300

    link in posts highlith

--- partials/highlight.php
+++ partials/highlight.php
@@ -12,28 +12,29 @@
         while ($query->have_posts()) : $query->the_post();
 ?>
         <section class="highlight <?= esc_attr($args['class'] ?? ''); ?> paddingContent" itemscope itemtype="http://schema.org/Article">
-            <article>
-                <span class="category" itemprop="articleSection">Destaque</span>
-                <?php the_title('<h1 itemprop="headline">', '</h1>'); ?>
-                <div itemprop="description">
-                    <?php the_excerpt(); ?>
-                </div>
-                <time datetime="<?= esc_attr(get_the_date('c')); ?>" itemprop="datePublished">
-                    <?= esc_html(get_the_date('d \d\e F \d\e Y')); ?>
-                </time>
-                <a href="<?php the_permalink();?>" title="Saiba mais sobre <?= esc_attr(get_the_title()); ?>" class="btnUrl">Saiba mais</a>
-            </article>
-            <figure itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-                <?php
-                if (has_post_thumbnail()) {
-                    the_post_thumbnail('posts_highlight', [
-                        'alt' => get_the_title(),
-                        'itemprop' => 'url',
-                        'loading' => 'lazy'
-                    ]);
-                }
-                ?>
-            </figure>
+            <a href="<?= esc_url(get_permalink()); ?>" title="<?php the_title(); ?>" itemprop="url">
+                <article>
+                    <span class="category" itemprop="articleSection">Destaque</span>
+                    <?php the_title('<h1 itemprop="headline">', '</h1>'); ?>
+                    <div itemprop="description">
+                        <p><?php echo wp_trim_words(get_the_content(), 30, '...'); ?></p>
+                    </div>
+                    <time datetime="<?= esc_attr(get_the_date('c')); ?>" itemprop="datePublished">
+                        <?= esc_html(get_the_date('d \d\e F \d\e Y')); ?>
+                    </time>
+                </article>
+                <figure itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+                    <?php
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail('posts_highlight', [
+                            'alt' => get_the_title(),
+                            'itemprop' => 'url',
+                            'loading' => 'lazy'
+                        ]);
+                    }
+                    ?>
+                </figure>
+            </a>
         </section>
 <?php
     endwhile;
