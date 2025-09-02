f5c671843776f2c702b719c570bfdf314e45414b
Rodrigo Vieira E. Silva <rodrigo.vesilva>
2025-09-02 15:04:38 -0300

    create css search

--- search.php
+++ search.php
@@ -2,5 +2,6 @@
     get_header();
     get_template_part('partials/breadcrumb');
     get_template_part('partials/sections/section-search');
+    get_template_part('partials/partners');
     get_footer();
 ?>
