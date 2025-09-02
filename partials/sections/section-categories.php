38ad15b13055607ee14bf7c5968aefd00de11854
Rodrigo Vieira E. Silva <rodrigo.vesilva>
2025-09-02 16:53:44 -0300

    link in posts search

---
+++ partials/sections/section-categories.php
@@ -0,0 +1,52 @@
+<section class="paddingContent">
+    <header>
+        <!-- Name category -->
+        <h1><?php single_cat_title(); ?></h1>
+        <span><?php echo category_description(); ?></span>
+
+        <div class="filters">
+            <select name="year_filter" id="year_filter">
+                <option value="">Todos os anos</option>
+                <?php
+                    $years = range(date('Y'), 2006);
+                    foreach ($years as $year) {
+                        echo '<option value="' . esc_attr($year) . '">' . esc_html($year) . '</option>';
+                    }
+                ?>
+            </select>
+
+            <select name="month_filter" id="month_filter">
+                <option value="">Todos os meses</option>
+                <?php
+                    $months_name = [
+                        1 => 'Janeiro',
+                        2 => 'Fevereiro',
+                        3 => 'Março',
+                        4 => 'Abril',
+                        5 => 'Maio',
+                        6 => 'Junho',
+                        7 => 'Julho',
+                        8 => 'Agosto',
+                        9 => 'Setembro',
+                        10 => 'Outubro',
+                        11 => 'Novembro',
+                        12 => 'Dezembro'
+                    ];
+                    foreach ($months_name as $month => $name) {
+                        echo '<option value="' . esc_attr($month) . '">' . esc_html($name) . '</option>';
+                    }
+                ?>
+            </select>
+
+            <select name="category_filter" id="category_filter">
+                <option value="">Todas as categorias</option>
+                <?php
+                    $categories = get_categories();
+                    foreach ($categories as $category) {
+                        echo '<option value="' . esc_attr($category->term_id) . '">' . esc_html($category->name) . '</option>';
+                    }
+                ?>
+            </select>
+        </div>
+    </header>
+</section>
