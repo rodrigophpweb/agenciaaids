8a54545577a08b0846e98364f737aea06e52b510
Rodrigo Vieira E. Silva <rodrigo.vesilva>
2025-09-02 16:15:36 -0300

    adjusment images

--- partials/sections/section-search.php
+++ partials/sections/section-search.php
@@ -12,7 +12,7 @@
                     <?php if (has_post_thumbnail()) : ?>
                         <?php the_post_thumbnail('thumbnail'); ?>
                     <?php else : ?>
-                        <img src="<?= esc_url('https://www.agenciaaids.com.br/wp-content/themes/assets/images/backdrop-ag-aids-compress-web.webp')?>" alt="<?= esc_attr(get_the_title()); ?>">
+                        <img src="https://www.agenciaaids.com.br/wp-content/themes/assets/images/backdrop-ag-aids-compress-web.webp" alt="<?= esc_attr(get_the_title()); ?>">
                     <?php endif; ?>
                 </figure>
 
