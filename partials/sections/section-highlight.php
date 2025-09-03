8592809bdb79cae0a0a4d59d800ae28240758edb
Rodrigo Vieira Eufrasio da Silva <rodrigo.mct@gmail.com>
2025-09-03 20:13:40 -0300

    remove date in highlight

--- partials/sections/section-highlight.php
+++ partials/sections/section-highlight.php
@@ -27,8 +27,7 @@
                         
                         <div class="post-info">
                             <?php the_title('<h3 itemprop="headline">','</h3>')?>
-                            <p itemprop="description"><?php echo wp_trim_words(get_the_content(), 70, '...'); ?></p>
-                            <time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished"><?php echo get_the_date(); ?></time>
+                            <p itemprop="description"><?php echo wp_trim_words(get_the_content(), 100, '...'); ?></p>
                             <meta itemprop="dateModified" content="<?php echo get_the_modified_date('c'); ?>">
                             <meta itemprop="author" content="<?php echo get_the_author(); ?>">
                         </div>
