0dd854dddc4677604070d118234218666f639cce
Rodrigo Vieira Eufrasio da Silva <rodrigo.mct@gmail.com>
2025-09-04 18:46:40 -0300

    the_content 100 caracters

--- partials/sections/section-highlight.php
+++ partials/sections/section-highlight.php
@@ -27,7 +27,7 @@
                         
                         <div class="post-info">
                             <?php the_title('<h3 itemprop="headline">','</h3>')?>
-                            <p itemprop="description"><?php echo wp_trim_words(trim(str_replace(['&nbsp;', ' '], ' ', get_the_content())), 30, '...'); ?></p>
+                            <p itemprop="description"><?php echo wp_trim_words(trim(str_replace(['&nbsp;', ' '], ' ', get_the_content())), 100, '...'); ?></p>
                             <meta itemprop="dateModified" content="<?php echo get_the_modified_date('c'); ?>">
                             <meta itemprop="author" content="<?php echo get_the_author(); ?>">
                         </div>
