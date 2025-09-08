02e47e7d6586810cbf6ef83d25749d8554f7ee22
Rodrigo Vieira E. Silva <rodrigo.vesilva>
2025-09-08 15:49:25 -0300

    insert author in single articles

--- single-artigos.php
+++ single-artigos.php
@@ -55,7 +55,18 @@
                 $post_url   = urlencode(get_permalink());
                 $post_title = urlencode(get_the_title());
                 $site_name  = urlencode(get_bloginfo('name'));
+                $autor = get_field('autor');
             ?>
+
+            <?php if ($autor): ?>
+                <div class="post-author" itemprop="author" itemscope itemtype="https://schema.org/Person" style="margin-bottom: 2rem; margin-top: 1rem;">
+                    <span class="author-label" style="font-weight: 600; color: #444;">Autor:</span>
+                    <span class="author-name" itemprop="name" style="font-weight: 400; color: #222; margin-left: 0.5em;">
+                        <?= esc_html($autor); ?>
+                    </span>
+                </div>
+            <?php endif; ?>
+
             <div class="share-buttons">
                 <strong>Compartilhar:</strong>
                 <a href="https://www.facebook.com/sharer/sharer.php?u=<?= $post_url ?>" target="_blank" rel="noopener" aria-label="Compartilhar no Facebook"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 440 146.7 540.8 258.2 568.5L258.2 398.2L205.4 398.2L205.4 320L258.2 320L258.2 286.3C258.2 199.2 297.6 158.8 383.2 158.8C399.4 158.8 427.4 162 438.9 165.2L438.9 236C432.9 235.4 422.4 235 409.3 235C367.3 235 351.1 250.9 351.1 292.2L351.1 320L434.7 320L420.3 398.2L351 398.2L351 574.1C477.8 558.8 576 450.9 576 320z"/></svg></a>
@@ -64,12 +75,9 @@
                 <a href="https://twitter.com/intent/tweet?url=<?= $post_url ?>&text=<?= $post_title ?>" target="_blank" rel="noopener" aria-label="Compartilhar no X (Twitter)"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M160 96C124.7 96 96 124.7 96 160L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 160C544 124.7 515.3 96 480 96L160 96zM457.1 180L353.3 298.6L475.4 460L379.8 460L305 362.1L219.3 460L171.8 460L282.8 333.1L165.7 180L263.7 180L331.4 269.5L409.6 180L457.1 180zM419.3 431.6L249.4 206.9L221.1 206.9L392.9 431.6L419.3 431.6z"/></svg></a>
                 <a href="https://www.linkedin.com/sharing/share-offsite/?url=<?= $post_url ?>" target="_blank" rel="noopener" aria-label="Compartilhar no LinkedIn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M160 96C124.7 96 96 124.7 96 160L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 160C544 124.7 515.3 96 480 96L160 96zM165 266.2L231.5 266.2L231.5 480L165 480L165 266.2zM236.7 198.5C236.7 219.8 219.5 237 198.2 237C176.9 237 159.7 219.8 159.7 198.5C159.7 177.2 176.9 160 198.2 160C219.5 160 236.7 177.2 236.7 198.5zM413.9 480L413.9 376C413.9 351.2 413.4 319.3 379.4 319.3C344.8 319.3 339.5 346.3 339.5 374.2L339.5 480L273.1 480L273.1 266.2L336.8 266.2L336.8 295.4L337.7 295.4C346.6 278.6 368.3 260.9 400.6 260.9C467.8 260.9 480.3 305.2 480.3 362.8L480.3 480L413.9 480z"/></svg></a>
                 <!-- <button type="button" onclick="copyToClipboard('<?=get_permalink();?>')" aria-label="Copiar link"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M288 64C252.7 64 224 92.7 224 128L224 384C224 419.3 252.7 448 288 448L480 448C515.3 448 544 419.3 544 384L544 183.4C544 166 536.9 149.3 524.3 137.2L466.6 81.8C454.7 70.4 438.8 64 422.3 64L288 64zM160 192C124.7 192 96 220.7 96 256L96 512C96 547.3 124.7 576 160 576L352 576C387.3 576 416 547.3 416 512L416 496L352 496L352 512L160 512L160 256L176 256L176 192L160 192z"/></svg></button> -->
-            </div>
-
-        </div>        
+            </div>      
     </article>
 <?php 
     get_template_part('', 'related-news');
     get_template_part('partials/partners');
     get_footer();
-?>
