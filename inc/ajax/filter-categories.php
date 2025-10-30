79e49ae076c957c0349552a69688c3d3b08344d7
Rodrigo Vieira E. Silva <rodrigo.vesilva>
2025-10-30 18:42:33 -0300

    ajuste test image ajax

--- inc/ajax/filter-categories.php
+++ inc/ajax/filter-categories.php
@@ -54,6 +54,7 @@ function agenciaaids_filter_posts() {
                         <?php if (has_post_thumbnail()) : ?>
                             <?php the_post_thumbnail('thumbnail'); ?>
                         <?php else : ?>
+                            <h2>test</h2>
                             <img src="https://agenciaaids.com.br/wp-content/themes/agenciaaids/assets/images/backdrop-ag-aids-compress-web.webp" alt="<?= esc_attr(get_the_title()); ?>">
                         <?php endif; ?>
                     </figure>
